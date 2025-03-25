62376c7782fe5ddadafb37ecdb19370c66868a16
Ibrahim el khalil <salabanziii89@gmail.com>
2025-03-25 01:14:07 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*----- TYPING EFFECT TEXT-----*/
+ let typingEffect = new Typed(".typedText",{
+    strings : ["and Graphic Designer","Based In Kenitra."],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 90,
+    backDelay : 2000
+ })
+  /*-----TYPING EFFECT TEXT-----*/
